04832ac7315c1afb922ad1776da078c489822320
Chris-Tromboss <ch972838@ucf.edu>
2023-11-03 17:27:06 -0400

    Updated PlayerBehavior

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -8,21 +8,23 @@ public class PlayerBehavior : MonoBehaviour
     //2. type (int, float, bool)
     //3. name (naming conventions) they always start with a letter that is not capitalized, but you can have multiple words with no spaces and following words could be capitalized
     //4. OPTIONAL: you can give it a value
-    //borders: 8.5 and 6.5
+    //borders: 14 (horizontal), 1 (halfway up screen), -3.5 (bottom of screen)
 
     public float speed;
     public float horizontalInput;
     public float verticalInput;
     public float horizontalScreenLimit;
     public float verticalScreenLimit;
+    public float bottomScreenLimit;
     public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 4f;
-        horizontalScreenLimit = 9.5f;
-        verticalScreenLimit = 6.5f;
+        horizontalScreenLimit = 14f;
+        verticalScreenLimit = 1f;
+        bottomScreenLimit = -3.5f;
     }
 
     // Update is called once per frame; if your computer runs at 60 fps
@@ -37,7 +39,7 @@ public class PlayerBehavior : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenLimit || transform.position.x < -horizontalScreenLimit)
+        if (transform.position.x > horizontalScreenLimit)
         {
             transform.position = new Vector3(-horizontalScreenLimit, transform.position.y, 0);
         }
@@ -48,11 +50,11 @@ public class PlayerBehavior : MonoBehaviour
 
         if (transform.position.y > verticalScreenLimit)
         {
-            transform.position = new Vector3(transform.position.x, -verticalScreenLimit, 0);
-        }
-        else if (transform.position.y < -verticalScreenLimit)
-        {
             transform.position = new Vector3(transform.position.x, verticalScreenLimit, 0);
+        }
+        else if (transform.position.y < bottomScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x, bottomScreenLimit, 0);
         }
     }
 
@@ -66,4 +68,5 @@ public class PlayerBehavior : MonoBehaviour
         }
     }
 }
+
 
